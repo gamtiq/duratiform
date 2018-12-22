1ecc4a49f4100eb1139a711832de976b83c00a65
Denis Sikuler <progwork@yandex.com>
2018-12-22 11:20:27 +0300

    Add types declaration file

---
+++ index.d.ts
@@ -0,0 +1,18 @@
+// Created on the basis of http://www.typescriptlang.org/docs/handbook/declaration-files/templates/module-d-ts.html
+
+export as namespace duratiform;
+
+export interface DivideResult {
+    day?: number;
+    day2?: string;
+    hour?: number;
+    hour2?: string;
+    minute?: number;
+    minute2?: string;
+    second?: number;
+    second2?: string;
+}
+
+export function divide(duration: number, partQty?: number, addStrings?: boolean): DivideResult;
+
+export function format(duration: number, format?: string): string;
